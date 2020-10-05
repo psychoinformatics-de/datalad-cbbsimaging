820d9350e5de9c203796e2ca9cd907e3e52e2ab7
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2020-10-05 12:39:47 +0200

    ENH: Allow demo Dataset to use configured toolbox URL

--- datalad_hirni/tests/utils.py
+++ datalad_hirni/tests/utils.py
@@ -13,26 +13,7 @@ from datalad.utils import (
 )
 from datalad.support.annexrepo import AnnexRepo
 from datalad.tests.utils import with_tempfile
-
-
-def install_demo_dataset(ds, path, recursive=False):
-    source = "https://github.com/psychoinformatics-de/hirni-demo"
-    if "datalad.hirni.toolbox.url" in cfg:
-        subds = ds.install(source=source,
-                           path=path,
-                           recursive=False)
-        # overwrite tool-box url
-        subds.repo.call_git(["config", "-f", ".gitmodules", "--replace-all",
-                          "submodule.code/hirni-toolbox.url",
-                          cfg.get("datalad.hirni.toolbox.url")])
-        subds.save(".gitmodules", message="Rewrite toolbox URLfor hirni tests")
-        ds.save(path, message="Rewrite toolbox URL for hirni tests")
-        # resume recursion (if any)
-        if recursive:
-            for s in subds.subdatasets():
-                subds.install(s['path'], recursive=True)
-    else:
-        ds.install(source=source, path=path, recursive=recursive)
+from datalad_hirni.tests import HIRNI_TOOLBOX_URL
 
 
 # TODO: ------------------------------------------------------------------------
@@ -276,3 +257,26 @@ def cached_url(f, url=None, keys=None):
         return f(*(arg + (new_url,)), **kw)
 
     return newfunc
+
+
+@cached_url(url="https://github.com/psychoinformatics-de/hirni-demo")
+def install_demo_dataset(ds, path, source, recursive=False):
+
+    toolbox_overwrite = ds.config.get("datalad.hirni.toolbox.url")
+
+    if toolbox_overwrite:
+        subds = ds.install(source=source,
+                           path=path,
+                           recursive=False)
+        # overwrite tool-box url
+        subds.repo.call_git(["config", "-f", ".gitmodules", "--replace-all",
+                             "submodule.code/hirni-toolbox.url",
+                             toolbox_overwrite])
+        subds.save(".gitmodules", message="Rewrite toolbox URL for hirni tests")
+        ds.save(path, message="Rewrite toolbox URL for hirni tests")
+        # resume recursion (if any)
+        if recursive:
+            for s in subds.subdatasets():
+                subds.install(s['path'], recursive=True)
+    else:
+        ds.install(source=source, path=path, recursive=recursive)
